1dea6cf9e8c61acb0bc421c8464c426c595c7508
Andrew Volostnykh <zighalt790@gmail.com>
2020-05-17 14:16:58 +0300

    Generation of buttons added

--- src/main/java/own/qualifying/client/Starter.java
+++ src/main/java/own/qualifying/client/Starter.java
@@ -1,6 +1,8 @@
 package own.qualifying.client;
 
-import org.apache.jasper.compiler.Node;
+import com.google.gwt.regexp.shared.RegExp;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
 import own.qualifying.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -10,13 +12,8 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+
+import java.util.Random;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -28,107 +25,69 @@ public class Starter implements EntryPoint {
 
   private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
-  //private VerticalPanel buttonsPannel = new VerticalPanel();
-
-  /**
-   * This is the entry point method.
-   */
   public void onModuleLoad() {
 
     final Button sendButton = new Button("Send");
-    final TextBox nameField = new TextBox();
-    nameField.setText("Quantity of numbers");
-    final Label errorLabel = new Label();
+    final TextBox numbersField = new TextBox();
 
-    // We can add style names to widgets
+    numbersField.addKeyPressHandler(event -> {
+      if(event.getNativeEvent().getKeyCode()!=KeyCodes.KEY_DELETE &&
+              event.getNativeEvent().getKeyCode()!=KeyCodes.KEY_BACKSPACE &&
+              event.getNativeEvent().getKeyCode()!=KeyCodes.KEY_LEFT &&
+              event.getNativeEvent().getKeyCode()!=KeyCodes.KEY_RIGHT){
+        String c = event.getCharCode()+"";
+        if(RegExp.compile("[^0-9]").test(c))
+          numbersField.cancelKey();
+      }
+    });
+
     sendButton.addStyleName("sendButton");
 
-    // Add the nameField and sendButton to the RootPanel
-    // Use RootPanel.get() to get the entire body element
-    RootPanel.get("nameFieldContainer").add(nameField);
+    RootPanel.get("nameFieldContainer").add(numbersField);
     RootPanel.get("sendButtonContainer").add(sendButton);
-    RootPanel.get("errorLabelContainer").add(errorLabel);
+
+    numbersField.setFocus(true);
+    numbersField.selectAll();
 
 
-    // Focus the cursor on the name field when the app loads
-    nameField.setFocus(true);
-    nameField.selectAll();
-
-
-//    // Create the popup dialog box
-//    final DialogBox dialogBox = new DialogBox();
-//    dialogBox.setText("Remote Procedure Call");
-//    dialogBox.setAnimationEnabled(true);
-//    final Button closeButton = new Button("Close");
-//    // We can set the id of a widget by accessing its Element
-//    closeButton.getElement().setId("closeButton");
-//    final Label textToServerLabel = new Label();
-//    final HTML serverResponseLabel = new HTML();
-//    VerticalPanel dialogVPanel = new VerticalPanel();
-//    dialogVPanel.addStyleName("dialogVPanel");
-//    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-//    dialogVPanel.add(textToServerLabel);
-//    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-//    dialogVPanel.add(serverResponseLabel);
-//    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-//    dialogVPanel.add(closeButton);
-//    dialogBox.setWidget(dialogVPanel);
-//
-//
-//    // Add a handler to close the DialogBox
-//    closeButton.addClickHandler(new ClickHandler() {
-//      public void onClick(ClickEvent event) {
-//        dialogBox.hide();
-//        sendButton.setEnabled(true);
-//        sendButton.setFocus(true);
-//      }
-//    });
-
-    // Create a handler for the sendButton and nameField
+    // Create a handler for the sendButton and numbersField
     class MyHandler implements ClickHandler, KeyUpHandler {
       /**
        * Fired when the user clicks on the sendButton.
       */
       public void onClick(ClickEvent event) {
-        String inputedText = nameField.getText(); // this works!!
+        Random random = new Random();
+        String inputedText = numbersField.getText(); // this works!!
+        int numberOfButtons = Integer.parseInt(inputedText);
+
+        if(inputedText.isEmpty()){ // check for empty string
+          return;
+        }
 
         RootPanel.get("nameFieldContainer").clear();
         RootPanel.get("sendButtonContainer").clear();
-        // if successful try to add some buttons ?
 
-        Button firstButton = new Button(inputedText);
-        RootPanel.get("nameFieldContainer").add(firstButton);
+        for(int i = 0; i < numberOfButtons; i++){
+          RootPanel.get("nameFieldContainer").add(new Button("" + random.nextInt(1000)));
+        }
+        
+        Button reset = new Button("reset");
+        reset.addClickHandler(ev -> Window.Location.reload());
+        RootPanel.get("functionality").add(reset);
       }
 
-      /**git 
-       * Fired when the user types in the nameField.
-      */
+      // useless
       public void onKeyUp(KeyUpEvent event) {
         if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
 
         }
       }
-
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-      */
-//      private void sendNameToServer() {
-//        // First, we validate the input.
-//        errorLabel.setText("");
-//        String textToServer = nameField.getText();
-//        if (!FieldVerifier.isValidName(textToServer)) {
-//          errorLabel.setText("Please enter at least four characters");
-//          return;
-//        }
-//
-//
-//      }
     }
 
     // Add a handler to send the name to the server
     MyHandler handler = new MyHandler();
     sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
+    numbersField.addKeyUpHandler(handler);
 
   }
 
